5568a973627706776dbacb9f51df91e574eafde6
Nihad Hrnjic <nhiap6@gmail.com>
2020-04-14 20:02:47 +0200

    Initial commit

---
+++ src/main/java/com/schedule/workout/workoutScheduler/WorkoutSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.schedule.workout.workoutScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/schedule/workout/workoutScheduler/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.schedule.workout.workoutScheduler.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping(value = "/test")
+    public String test(){
+        return "Hello world";
+    }
+}
